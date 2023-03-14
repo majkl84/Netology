1654a2d84e559a3526be5cddb3696518c9f8438a
majkl84 <34282210+majkl84@users.noreply.github.com>
2023-03-14 15:35:49 +0300

    Проработка ДЗ

--- OOP Inheritance, encapsulation and polymorphism/main.py
+++ OOP Inheritance, encapsulation and polymorphism/main.py
@@ -19,8 +19,11 @@ class Student:
         return sum_grades
 
     def average_student(self):
-        average_grade = sum(Student.grades(self)) / len(Student.grades(self))
-        return round(average_grade, 1)
+        try:
+            average_grade = sum(Student.grades(self)) / len(Student.grades(self))
+            return round(average_grade, 1)
+        except ZeroDivisionError:
+            print("Нет оценок")
     def rate_lec(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and \
                 course in lecturer.courses_attached and course in self.courses_in_progress and 1 <= grade <= 10:
@@ -31,12 +34,9 @@ class Student:
         else:
             return 'Ошибка'
     def __str__(self):
-        try:
-            print(
+       print(
                 f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {Student.average_student(self)}"
                 f"\nКурсы в процессе изучения: {','.join(self.courses_in_progress)}\nКурсы завершены: {','.join(self.finished_courses)}")
-        except ZeroDivisionError:
-            print("Нет курсов")
 
     def __lt__(self, other):
         if self.average_student() >= other.average_student():
@@ -66,8 +66,11 @@ class Lecturer(Mentor):
         return sum_grades
 
     def average(self):
-        average_grade = sum(Lecturer.grades(self)) / len(Lecturer.grades(self))
-        return round(average_grade, 1)
+        try:
+            average_grade = sum(Lecturer.grades(self)) / len(Lecturer.grades(self))
+            return round(average_grade, 1)
+        except ZeroDivisionError:
+            print("Нет курсов")
 
     def __lt__(self, other):
         if self.average() >= other.average():
@@ -76,10 +79,8 @@ class Lecturer(Mentor):
             return f"{self.name} {self.surname} средняя оценка меньше, равно {self.average()}"
 
     def __str__(self):
-        try:
-            print(f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {Lecturer.average(self)}")
-        except ZeroDivisionError:
-            print("нет лекций")
+        print(f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {Lecturer.average(self)}")
+
 class Reviewer(Mentor):
 
     def __init__(self, name, surname):
@@ -98,13 +99,21 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 def courses_average_students(student_list, course):
-
+    if not isinstance(student_list, Student):
+        return "Нет студента"
+    estimation = []
     for student in student_list:
-        for k, v in student.grades.items():
-            if course == k:
-                sum_average = sum(v) / len(v)
-                print(f"Студент: {student.name} {student.surname}\nКурс: {k}\n"
-                      f"Cредняя оценка за домашние задания: {round(sum_average, 1)}\n")
+        estimation.extend(student.grades.get(course, []))
+    if not estimation:
+        return "По такому курсу нет оценок"
+    return round(sum(estimation) / len(estimation), 2)
+
+    # for student in student_list:
+    #     for k, v in student.grades.items():
+    #         if course == k:
+    #             sum_average = sum(v) / len(v)
+    #             print(f"Студент: {student.name} {student.surname}\nКурс: {k}\n"
+    #                   f"Cредняя оценка за домашние задания: {round(sum_average, 1)}\n")
 
 def courses_average_lecturer(lecturer_list, course):
     for lecturer in lecturer_list:
@@ -153,16 +162,14 @@ ivan_student.rate_lec(Gena_lecturer, 'Java', 5)
 ivan_student.rate_lec(Gena_lecturer, 'Java', 8)
 ivan_student.rate_lec(Gena_lecturer, 'Java', 6)
 
-# print(ruoy_student.grades)
-# print(Some_mentor.courses_attached)
-# print(Dima_lecturer.grades)
-Some_mentor.__str__()
-Dima_lecturer.__str__()
-ruoy_student.__str__()
+print(ruoy_student.grades)
+print(Some_mentor.courses_attached)
+print(Dima_lecturer.grades)
+
 
 print("")
 courses_average_students(Student.student_list, 'Python')
-courses_average_lecturer(Lecturer.lecturer_list, 'Python')
-
-print(Dima_lecturer > Gena_lecturer)
-print(ruoy_student < ivan_student)
+# courses_average_lecturer(Lecturer.lecturer_list, 'Python')
+#
+# print(Dima_lecturer > Gena_lecturer)
+# print(ruoy_student < ivan_student)
